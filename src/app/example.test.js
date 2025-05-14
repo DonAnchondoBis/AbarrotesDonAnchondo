ed5e36a4bf9e2f63cb334d00cb2c96a185727a9f
Salvador511 <salvadororozcogon511@gmail.com>
2025-05-14 14:00:43 -0600

    chore: add initial Vitest configuration and example test file

---
+++ src/app/example.test.js
@@ -0,0 +1,9 @@
+// TODO: Delete this file if not needed
+// This is a simple test file to demonstrate the testing setup.
+import { describe, it, expect } from 'vitest'
+
+describe('Example Test', () => {
+  it('should add numbers correctly', () => {
+    expect(1 + 1).toBe(2)
+  })
+})
---
+++ vitest.config.js
@@ -0,0 +1,32 @@
+import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: [
+      { find: '~/app', replacement: path.resolve(__dirname, 'src/app') },
+      { find: '~/api', replacement: path.resolve(__dirname, 'src/app/api') },
+      { find: '~/', replacement: path.resolve(__dirname, 'src/') },
+      { find: '~/Libs', replacement: path.resolve(__dirname, 'src/Libs') },
+    ],
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    coverage: {
+      provider: 'v8',
+      reportsDirectory: './coverage',
+      all: true,
+      reporter: ['text', 'json-summary', 'json'],
+      reportOnFailure: true,
+    },
+    exclude: [
+      'node_modules/**',
+      '**/playwright.config.js',
+      '**/next.config.mjs',
+      '**/tests/**',
+    ],
+  },
+})
