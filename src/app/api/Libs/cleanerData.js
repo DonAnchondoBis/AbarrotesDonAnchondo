9a385b2e9251877357d9e1c0ed0b6d5eb86cee94
Salvador511 <salvadororozcogon511@gmail.com>
2025-06-01 13:07:59 -0600

    fix: remove 'active' from OMIT_DATA in cleanerData utility

--- src/app/api/Libs/cleanerData.js
+++ src/app/api/Libs/cleanerData.js
@@ -1,6 +1,6 @@
 import { omit } from 'ramda'
 
-const OMIT_DATA = ['active', 'createdAt', 'password','updatedAt']
+const OMIT_DATA = ['createdAt', 'password','updatedAt']
 
 const cleanerData = ({ payload, omitProps = [] }) => {
   return omit([...OMIT_DATA, ...omitProps], payload)
