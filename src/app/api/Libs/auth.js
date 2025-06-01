db745fe6084d70c20f2aa04c41e6797cb2b925fe
Salvador511 <salvadororozcogon511@gmail.com>
2025-06-01 13:07:53 -0600

    fix: update authenticateToken to return userId and role from JWT

--- src/app/api/Libs/auth.js
+++ src/app/api/Libs/auth.js
@@ -4,8 +4,8 @@ export const authenticateToken = ({ headers }) => {
   const authHeader = headers.get('authorization')
   const token = authHeader && authHeader.replace(/Bearer /,'')
   if (token) {
-    const { professorId } = jwt.verify(token, process.env.JWT_SECRET)
-    return professorId
+    const { userId, role } = jwt.verify(token, process.env.JWT_SECRET)
+    return { userId, role }
   }
   return null
 }
