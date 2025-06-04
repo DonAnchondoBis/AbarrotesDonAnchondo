bd989958e73d5bb2beb7850225c7c09c8e594fa9
RaulMun <raulmuniz38@gmail.com>
2025-06-04 10:54:43 -0600

    feat: enhance ticket creation logic with user authentication

--- src/app/api/ticket/route.js
+++ src/app/api/ticket/route.js
@@ -8,13 +8,14 @@ import validatorFields from '~/app/api/Libs/validatorFields'
 import { EMPTY_OBJECT } from '~/app/Lib/Utils/constants'
 import prisma from '~/app/api/Libs/prisma'
 
+//Post method for a ticket
 export const POST = async request => {
   try{
-    const hasPermission = authenticateToken(request)
+    const { userId, role } = authenticateToken(request)
     const data = await request.json()
     const isValid = validatorFields({ data, shape: Ticket.shape })
 
-    if(hasPermission && isValid){
+    if(userId && isValid && (role == 'ADMIN' || role == 'CASHIER')){
       const payload = await prisma.ticket.create({
         data: {
           products: {
@@ -23,13 +24,35 @@ export const POST = async request => {
               quantityProduct: product.quantity
             }))
           },
-          ...data
+          userId,
+          total: data.total,
         }
       })
       const response = cleanerData({ payload })
       return NextResponse.json(response, { status: 201 })
     }
     return ERROR.FORBIDDEN()
+  } catch (error) {
+    return NextResponse.json({ error: error.message }, { status: error.status || 500 })
+  }
+}
+
+// Get method for tickets
+export const GET = async request => {
+  try{
+    const { role } = authenticateToken(request)
+    if(role != 'ADMIN' || role != 'CASHIER') return ERROR.FORBIDDEN()
+    const filter = Object.fromEntries(request.nextUrl.searchParams.entries())
+    const payloads = await prisma.ticket.findMany({
+      where: {
+        ...(filter)
+      }
+    })
+    if(payloads.length > 0){
+      const response = payloads.map(payload => cleanerData({ payload }))
+      return NextResponse.json(response, { status: 200 })
+    }
+    return ERROR.NOT_FOUND()
   } catch (error) {
     return NextResponse.json({ error: error.message }, { status: error.status || 500 })
   }
