466439a0973214d15f0d3f29d4b052b11575ff36
KamenDeku <alonsocorral2004@gmail.com>
2025-06-05 19:51:19 -0600

    feat: add GET route for ticket retrieval with authorization and error handling

---
+++ src/app/api/ticket/[id]/route.js
@@ -0,0 +1,28 @@
+import { NextResponse } from 'next/server'
+//import { Ticket } from '~/api/entities'
+import { authenticateToken } from '~/app/api/Libs/auth'
+import ERROR from '~/Libs/error'
+import cleanerData from '~/app/api/Libs/cleanerData'
+//import validatorFields from '~/app/api/Libs/validatorFields'
+import prisma from '~/app/api/Libs/prisma'
+
+export const GET = async (request, { params }) => {
+  try {
+    const { role } = authenticateToken(request) ?? {}
+    if (role !== 'ADMIN') return ERROR.FORBIDDEN()
+    const { id } = params
+    if (!Number(id)) return ERROR.INVALID_FIELDS()
+    const payload = await prisma.ticket.findUnique({
+      where: {
+        id: Number(id),
+      }
+    })
+    if (payload){
+      const response = cleanerData({ payload })
+      return NextResponse.json(response, { status: 200 })
+    }
+    return ERROR.NOT_FOUND()
+  } catch (error) {
+    return NextResponse.json({ error: error.message }, { status: error.status || 500 })
+  }
+}
