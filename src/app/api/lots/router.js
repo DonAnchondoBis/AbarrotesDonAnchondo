8eabc672fd91f03d83484350b27780a5906e36cd
Salvador511 <salvadororozcogon511@gmail.com>
2025-06-09 09:52:40 -0600

    fix: standardize code formatting and improve readability in lots and suppliers routers

--- src/app/api/lots/router.js
+++ src/app/api/lots/router.js
@@ -1,17 +1,17 @@
-import { NextResponse } from "next/server";
-import { Lots } from "~/api/entities";
-import { authenticateToken } from "~/app/api/Libs/auth";
-import ERROR from "~/Libs/error";
-import cleanerData from "~/app/api/Libs/cleanerData";
-import validatorFields from "~/app/api/Libs/validatorFields";
-import prisma from "~/app/api/Libs/prisma";
+import { NextResponse } from 'next/server'
+import { Lots } from '~/api/entities'
+import { authenticateToken } from '~/app/api/Libs/auth'
+import ERROR from '~/Libs/error'
+import cleanerData from '~/app/api/Libs/cleanerData'
+import validatorFields from '~/app/api/Libs/validatorFields'
+import prisma from '~/app/api/Libs/prisma'
 
-export const POST = async (request) => {
+export const POST = async request => {
   try {
-    const { role = null, userId } = authenticateToken(request) ?? {};
-    if (role !== "ADMIN" || !userId) return ERROR.FORBIDDEN();
-    const data = await request.json();
-    const isValid = validatorFields({ data, shape: Lots.shape });
+    const { role = null, userId } = authenticateToken(request) ?? {}
+    if (role !== 'ADMIN' || !userId) return ERROR.FORBIDDEN()
+    const data = await request.json()
+    const isValid = validatorFields({ data, shape: Lots.shape })
 
     if (isValid) {
       const product = await prisma.product.findUnique({
@@ -20,8 +20,8 @@ export const POST = async (request) => {
             id: data.id,
           },
         },
-      });
-      if (!product) return ERROR.NOT_FOUND("Product not found");
+      })
+      if (!product) return ERROR.NOT_FOUND()
 
       const payload = await prisma.lots.create({
         data: {
@@ -33,23 +33,23 @@ export const POST = async (request) => {
         include: {
           product: true,
         },
-      });
-      const response = cleanerData({ payload });
-      return NextResponse.json(response, { status: 201 });
+      })
+      const response = cleanerData({ payload })
+      return NextResponse.json(response, { status: 201 })
     }
-    return ERROR.FORBIDDEN();
+    return ERROR.FORBIDDEN()
   } catch (error) {
     return NextResponse.json(
       { error: error.message },
       { status: error.status || 500 }
-    );
+    )
   }
-};
+}
 
-export const GET = async (request) => {
+export const GET = async request => {
   try {
-    const { role, userId } = authenticateToken(request) ?? {};
-    if (role !== "ADMIN" || !userId) return ERROR.FORBIDDEN();
+    const { role, userId } = authenticateToken(request) ?? {}
+    if (role !== 'ADMIN' || !userId) return ERROR.FORBIDDEN()
     const payloads = await prisma.lots.findMany({
       select: {
         id: true,
@@ -59,17 +59,17 @@ export const GET = async (request) => {
         currentAmount: true,
         expirationDate: true,
       },
-    });
+    })
 
     if (payloads.length > 0) {
-      const response = payloads.map((payload) => cleanerData({ payload }));
-      return NextResponse.json(response, { status: 200 });
+      const response = payloads.map(payload => cleanerData({ payload }))
+      return NextResponse.json(response, { status: 200 })
     }
-    return ERROR.NOT_FOUND();
+    return ERROR.NOT_FOUND()
   } catch (error) {
     return NextResponse.json(
       { error: error.message },
       { status: error.status || 500 }
-    );
+    )
   }
-};
+}
