304004a2c36d2b76678805568181f72eb8c2b538
Maydelin López <141065267+maydelpz@users.noreply.github.com>
2025-06-10 10:20:08 -0600

    feat: endpoint de suppliers y lots modificados

--- src/app/api/entities.js
+++ src/app/api/entities.js
@@ -1,58 +1,36 @@
 export const StoreInfo = {
-  name: 'storeInfo',
-  shape: [
-    'name',
-    'address',
-    'phone',
-    'dollarValue',
-    'yenValue',
-  ]
-}
+  name: "storeInfo",
+  shape: ["name", "address", "phone", "dollarValue", "yenValue"],
+};
 
 export const Ticket = {
-  name: 'tickets',
-  shape: [
-    'products',
-    'total'
-  ]
-}
+  name: "tickets",
+  shape: ["products", "total"],
+};
 
 export const User = {
-  name: 'user',
-  shape: [
-    'name',
-    'username',
-    'password',
-    'role'
-  ]
-}
+  name: "user",
+  shape: ["name", "username", "password", "role"],
+};
 
 export const InventoryLog = {
-  name: 'inventoryLog',
+  name: "inventoryLog",
   shape: [
-    'productName',
-    'amount',
-    'description',
-    'expirationDate',
-    'type',
-    'userId'
-  ]
-}
+    "productName",
+    "amount",
+    "description",
+    "expirationDate",
+    "type",
+    "userId",
+  ],
+};
 
 export const Supplier = {
-  name: 'supplier',
-  shape: [
-    'name',
-    'phone',
-    'email'
-  ]
-}
+  name: "supplier",
+  shape: ["name", "phone", "email"],
+};
 
 export const Lots = {
-  name: 'lots',
-  shape: [
-    'productId',
-    'initialAmount',
-    'expirationDate'
-  ]
-}
+  name: "lots",
+  shape: ["productId", "initialAmount", "expirationDate"],
+};
