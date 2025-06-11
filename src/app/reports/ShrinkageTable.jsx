ccfac4f4f9ffbfc907b782b2152639fbe574101a
Salvador511 <salvadororozcogon511@gmail.com>
2025-06-10 21:02:16 -0600

    feat: implement SalesOfTheDayTable and ShrinkageTable components with pagination and search functionality

--- src/app/reports/ShrinkageTable.jsx
+++ src/app/reports/ShrinkageTable.jsx
@@ -1,3 +1,5 @@
+'use client'
+
 import {
   Table,
   TableBody,
@@ -5,32 +7,35 @@ import {
   TableHead,
   TableRow,
   Paper,
-  Typography,
+  Typography as T,
   TablePagination,
   TableContainer,
+  Tooltip
 } from '@mui/material'
 import { styled } from '@mui/material/styles'
 import { useState } from 'react'
+import getClassPrefixer from '~/app/UI/classPrefixer'
+
+
+const displayName = 'ShrinkageTable'
+const classes = getClassPrefixer(displayName)
 
 const StyledTableContainer = styled(TableContainer)(({ theme }) => ({
   width: '100%',
   border: `solid 3px ${theme.palette.primary.main}`,
   borderRadius: '1rem',
+  [`& .${classes.descriptionText}`]: {
+    maxWidth: '200px',
+    textAlign: 'center',
+    margin: '0 auto',
+  },
 }))
 
-const ShrinkageTable = ({ search }) => {
-  const data = [
-    { product: 'Keyboard', amount: 25, location: 'Warehouse A' },
-    { product: 'Mouse', amount: 40, location: 'Warehouse B' }
-  ]
-
-  const filteredData = data.filter(item => 
-    item.product.toLowerCase().includes(search.toLowerCase())
-  )
-
+const ShrinkageTable = ({ data = [], date = '' }) => {
   const [page, setPage] = useState(0)
   const [rowsPerPage, setRowsPerPage] = useState(5)
-  const paginatedRows = filteredData.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+  const shrinkageData = data.filter(item => item?.date === date)
+  const paginatedRows = shrinkageData?.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
 
   const handleChangePage = (_, newPage) => setPage(newPage)
   const handleChangeRowsPerPage = event => {
@@ -44,28 +49,58 @@ const ShrinkageTable = ({ search }) => {
         <TableHead>
           <TableRow>
             <TableCell align="center">
-              <Typography color="primary.main" fontWeight="bold">
+              <T color="primary.main" fontWeight="bold">
                 Product
-              </Typography>
+              </T>
             </TableCell>
             <TableCell align="center">
-              <Typography color="primary.main" fontWeight="bold">
+              <T color="primary.main" fontWeight="bold">
                 Amount
-              </Typography>
+              </T>
             </TableCell>
             <TableCell align="center">
-              <Typography color="primary.main" fontWeight="bold">
-                Location
-              </Typography>
+              <T color="primary.main" fontWeight="bold">
+                Description
+              </T>
+            </TableCell>
+            <TableCell align="center">
+              <T color="primary.main" fontWeight="bold">
+                Expiration Date
+              </T>
+            </TableCell>
+            <TableCell align="center">
+              <T color="primary.main" fontWeight="bold">
+                Date of Generation
+              </T>
             </TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {paginatedRows.map((row, index) => (
             <TableRow key={index}>
-              <TableCell align="center">{row.product}</TableCell>
-              <TableCell align="center">{row.amount}</TableCell>
-              <TableCell align="center">{row.location}</TableCell>
+              <TableCell align="center">{row?.productName}</TableCell>
+              <TableCell align="center">{row?.amount}</TableCell>
+              <TableCell align="center">
+                <Tooltip
+                  title={row?.description || ''}
+                  arrow
+                  placement="top"
+                  slotProps={{
+                    tooltip: {
+                      sx: {
+                        backgroundColor: theme => theme.palette.contrast.main,
+                        color: theme => theme.palette.background.main,
+                      },
+                    },
+                  }}
+                >
+                  <T noWrap className={classes.descriptionText}>
+                    {row?.description}
+                  </T>
+                </Tooltip>
+              </TableCell>
+              <TableCell align="center">{row?.expirationDate}</TableCell>
+              <TableCell align="center">{row?.date}</TableCell>
             </TableRow>
           ))}
         </TableBody>
@@ -73,7 +108,7 @@ const ShrinkageTable = ({ search }) => {
       <TablePagination
         rowsPerPageOptions={[5, 10, 25]}
         component="div"
-        count={data.length}
+        count={data?.length}
         rowsPerPage={rowsPerPage}
         page={page}
         onPageChange={handleChangePage}
--- src/app/reports/page.jsx
+++ src/app/reports/page.jsx
@@ -4,18 +4,26 @@ import {
   Tab,
   OutlinedInput,
   InputAdornment,
+  TextField,
+  Typography as T,
 } from '@mui/material'
-import { useState } from 'react'
 import { styled } from '@mui/material/styles'
 import SearchIcon from '@mui/icons-material/Search'
 import getClassPrefixer from '~/app/UI/classPrefixer'
+
 import InventoryTable from './InventoryTable'
 import ShrinkageTable from './ShrinkageTable'
-import SalesOfTheDayTable from './SaleDay'
+import SalesOfTheDayTable from './SalesOfTheDayTable'
 
-const displayName = 'Competitors'
+import { useEffect, useState } from 'react'
+
+import apiFetch from '~/app/Lib/apiFetch'
+import { useToken } from '~/app/store/useToken'
+import Loading from '~/app/UI/Shared/Loading'
+
+const displayName = 'ReportsPage'
 const classes = getClassPrefixer(displayName)
-const Container = styled('div')(() => ({
+const Container = styled('div')(({ theme }) => ({
   display: 'flex',
   flexDirection: 'column',
   padding: '2rem 8rem',
@@ -39,14 +47,40 @@ const Container = styled('div')(() => ({
   [`& .${classes.searchInput}`]: {
     minWidth: '250px',
     marginLeft: '1rem',
+    backgroundColor: theme.palette.contrast.main,
+    '& input::placeholder': {
+      color: theme.palette.background.main,
+      opacity: 0.7
+    },
+    '& .MuiInputBase-input': {
+      color: theme.palette.background.main,
+    }
+  },
+  [`& .${classes.dateInput}`]: {
+    minWidth: '200px',
+    backgroundColor: theme.palette.contrast.main,
+    '& .MuiInputBase-input': {
+      color: theme.palette.background.main,
+    },
+    '& .MuiInputLabel-root': {
+      color: theme.palette.background.main,
+      opacity: 0.7
+    },
   },
 }))
 
-const PublicTable = ({
+const ReportsPage = ({
   selectedCategory,
   handleCategoryChange,
   search,
   handleSearchChange,
+  shrinkage,
+  isLoading,
+  selectedDateShrinkage,
+  handleDateChangeShrinkage,
+  selectedDateSalesday,
+  handleDateChangeSalesday,
+  error
 }) => {
   return (
     <Container>
@@ -56,49 +90,130 @@ const PublicTable = ({
           onChange={handleCategoryChange}
           indicatorColor="primary"
           textColor="primary"
-          
+          allowScrollButtonsMobile
+          variant="scrollable"
         >
           <Tab label="Inventory" value="inventory" />
           <Tab label="Shrinkage" value="shrinkage" />
           <Tab label="Sales of the day" value="salesday" />
         </Tabs>
-        <OutlinedInput
-          className={classes.searchInput}
-          placeholder="Search by name"
-          size="small"
-          value={search}
-          onChange={handleSearchChange}
-          startAdornment={
-            <InputAdornment position="start">
-              <SearchIcon />
-            </InputAdornment>
-          }
-        />
+        {selectedCategory === 'inventory' && !error && (
+          <OutlinedInput
+            className={classes.searchInput}
+            placeholder="Search by name"
+            size="small"
+            value={search}
+            onChange={handleSearchChange}
+            startAdornment={
+              <InputAdornment position="start">
+                <SearchIcon color='background'/>
+              </InputAdornment>
+            }
+          />
+        )}
+        {selectedCategory === 'shrinkage' && (
+          <TextField
+            variant="filled"
+            name="date"
+            label="Seleccionar fecha"
+            type="date"
+            value={selectedDateShrinkage ?? ''}
+            onChange={handleDateChangeShrinkage}
+            InputLabelProps={{
+              shrink: true,
+            }}
+            className={classes.dateInput}
+          />
+        )}
+        {selectedCategory === 'salesday' && (
+          <TextField
+            variant="filled"
+            name="date"
+            label="Seleccionar fecha"
+            type="date"
+            value={selectedDateSalesday ?? ''}
+            onChange={handleDateChangeSalesday}
+            InputLabelProps={{
+              shrink: true,
+            }}
+            className={classes.dateInput}
+          />
+        )}
       </div>
-      <div className={classes.tableContainer}>
-        {selectedCategory === 'inventory' && <InventoryTable search={search} />}
-        {selectedCategory === 'shrinkage' && <ShrinkageTable search={search} />}
-        {selectedCategory === 'salesday' && <SalesOfTheDayTable search={search} />}
-      </div>
+      {isLoading
+        ? (<Loading />)
+        : (
+          <div className={classes.tableContainer}>
+            {selectedCategory === 'inventory' && !error && <InventoryTable search={search} />}
+            {selectedCategory === 'shrinkage' && !error && selectedDateShrinkage && <ShrinkageTable data={shrinkage} date={selectedDateShrinkage} />}
+            {selectedCategory === 'salesday' && !error && selectedDateSalesday && <SalesOfTheDayTable search={search} />}
+            {error && (
+              <T color="error" variant="h6">
+                Sorry there was an error, please try again later.
+              </T>
+            )}
+            {(!selectedDateSalesday) && (selectedCategory === 'salesday') && (
+              <T variant="h6">
+                Select a date, please.
+              </T>
+            )}
+            {(!selectedDateShrinkage) && (selectedCategory === 'shrinkage') && (
+              <T variant="h6">
+                Select a date, please.
+              </T>
+            )}
+          </div>
+        )
+      }
     </Container>
   )
 }
 
 const Wrapper = () => {
   const [selectedCategory, setSelectedCategory] = useState('inventory')
+  const [selectedDateShrinkage, setSelectedDateShrinkage] = useState(null)
+  const [selectedDateSalesday, setSelectedDateSalesday] = useState(null)
+  const [error, setError] = useState(false)
+  const [shrinkage, setShrinkage] = useState([])
   const [search, setSearch] = useState('')
+  const [isLoading, setIsLoading] = useState(true)
+  const { token } = useToken()
 
+  const handleDateChangeShrinkage = event => setSelectedDateShrinkage(event.target.value)
+  const handleDateChangeSalesday = event => setSelectedDateSalesday(event.target.value)
   const handleCategoryChange = (_, newValue) => setSelectedCategory(newValue)
   const handleSearchChange = event => setSearch(event.target.value)
 
+  useEffect(() => {
+    setIsLoading(true)
+    const getShrinkage = async () => {
+      const response = await apiFetch({ url: 'api/inventoryLog?type=SHRINKAGE', method: 'GET', token })
+      if (response.error && response.error !== 'Not allowed') {
+        setError(true)
+        setShrinkage([])
+      } else {
+        setError(false)
+        setShrinkage(response)
+      }
+      setIsLoading(false)
+    }
+    getShrinkage()
+  }, [token])
+
   return (
-    <PublicTable
+    <ReportsPage
       selectedCategory={selectedCategory}
       handleCategoryChange={handleCategoryChange}
       search={search}
       handleSearchChange={handleSearchChange}
-      
-    />  
+      shrinkage={shrinkage}
+      isLoading={isLoading}
+      error={error}
+      selectedDateShrinkage={selectedDateShrinkage}
+      handleDateChangeShrinkage={handleDateChangeShrinkage}
+      selectedDateSalesday={selectedDateSalesday}
+      handleDateChangeSalesday={handleDateChangeSalesday}
+    />
   )
 }
 
