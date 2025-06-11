cb36cdccf7c84c4e24e36253f48ce17a8d9467f6
Salvador511 <salvadororozcogon511@gmail.com>
2025-06-11 13:33:55 -0600

    feat: update ShrinkageTable with styled container and add Generate Report button

--- src/app/reports/ShrinkageTable.jsx
+++ src/app/reports/ShrinkageTable.jsx
@@ -10,7 +10,8 @@ import {
   Typography as T,
   TablePagination,
   TableContainer,
-  Tooltip
+  Tooltip,
+  Button,
 } from '@mui/material'
 import { styled } from '@mui/material/styles'
 import { useState } from 'react'
@@ -20,14 +21,23 @@ import getClassPrefixer from '~/app/UI/classPrefixer'
 const displayName = 'ShrinkageTable'
 const classes = getClassPrefixer(displayName)
 
-const StyledTableContainer = styled(TableContainer)(({ theme }) => ({
-  width: '100%',
-  border: `solid 3px ${theme.palette.primary.main}`,
-  borderRadius: '1rem',
+const Container = styled('div')(({ theme }) => ({
+  [`& .${classes.TableContainer}`]: {
+    width: '100%',
+    border: `solid 3px ${theme.palette.primary.main}`,
+    borderRadius: '1rem',
+  },
   [`& .${classes.descriptionText}`]: {
     maxWidth: '200px',
     textAlign: 'center',
     margin: '0 auto',
+  },
+  [`& .${classes.buttonContainer}`]: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+    width: '100%',
+    marginTop: '1rem',
+    color: theme.palette.background.main,
   },
 }))
 
@@ -44,78 +54,86 @@ const ShrinkageTable = ({ data = [], date = '' }) => {
   }
 
   return (
-    <StyledTableContainer component={Paper}>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell align="center">
-              <T color="primary.main" fontWeight="bold">
-                Product
-              </T>
-            </TableCell>
-            <TableCell align="center">
-              <T color="primary.main" fontWeight="bold">
-                Amount
-              </T>
-            </TableCell>
-            <TableCell align="center">
-              <T color="primary.main" fontWeight="bold">
-                Description
-              </T>
-            </TableCell>
-            <TableCell align="center">
-              <T color="primary.main" fontWeight="bold">
-                Expiration Date
-              </T>
-            </TableCell>
-            <TableCell align="center">
-              <T color="primary.main" fontWeight="bold">
-                Date of Generation
-              </T>
-            </TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {paginatedRows.map((row, index) => (
-            <TableRow key={index}>
-              <TableCell align="center">{row?.productName}</TableCell>
-              <TableCell align="center">{row?.amount}</TableCell>
+    <Container>
+      <TableContainer className={classes.TableContainer} component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
               <TableCell align="center">
-                <Tooltip
-                  title={row?.description || ''}
-                  arrow
-                  placement="top"
-                  slotProps={{
-                    tooltip: {
-                      sx: {
-                        backgroundColor: theme => theme.palette.contrast.main,
-                        color: theme => theme.palette.background.main,
-                      },
-                    },
-                  }}
-                >
-                  <T noWrap className={classes.descriptionText}>
-                    {row?.description}
-                  </T>
-                </Tooltip>
+                <T color="primary.main" fontWeight="bold">
+                  Product
+                </T>
               </TableCell>
-              <TableCell align="center">{row?.expirationDate}</TableCell>
-              <TableCell align="center">{row?.date}</TableCell>
+              <TableCell align="center">
+                <T color="primary.main" fontWeight="bold">
+                  Amount
+                </T>
+              </TableCell>
+              <TableCell align="center">
+                <T color="primary.main" fontWeight="bold">
+                  Description
+                </T>
+              </TableCell>
+              <TableCell align="center">
+                <T color="primary.main" fontWeight="bold">
+                  Expiration Date
+                </T>
+              </TableCell>
+              <TableCell align="center">
+                <T color="primary.main" fontWeight="bold">
+                  Date of Generation
+                </T>
+              </TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-      <TablePagination
-        rowsPerPageOptions={[5, 10, 25]}
-        component="div"
-        count={data?.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onPageChange={handleChangePage}
-        onRowsPerPageChange={handleChangeRowsPerPage}
-        labelRowsPerPage="Products per page"
-      />
-    </StyledTableContainer>
+          </TableHead>
+          <TableBody>
+            {paginatedRows.map((row, index) => (
+              <TableRow key={index}>
+                <TableCell align="center">{row?.productName}</TableCell>
+                <TableCell align="center">{row?.amount}</TableCell>
+                <TableCell align="center">
+                  <Tooltip
+                    title={row?.description || ''}
+                    arrow
+                    placement="top"
+                    slotProps={{
+                      tooltip: {
+                        sx: {
+                          backgroundColor: theme => theme.palette.contrast.main,
+                          color: theme => theme.palette.background.main,
+                        },
+                      },
+                    }}
+                  >
+                    <T noWrap className={classes.descriptionText}>
+                      {row?.description}
+                    </T>
+                  </Tooltip>
+                </TableCell>
+                <TableCell align="center">{row?.expirationDate}</TableCell>
+                <TableCell align="center">{row?.date}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+        <TablePagination
+          rowsPerPageOptions={[5, 10, 25]}
+          component="div"
+          count={data?.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+          labelRowsPerPage="Products per page"
+        />
+      </TableContainer>
+      <div className={classes.buttonContainer}>
+        {/* TODO ADD Dani's function to generate reports */}
+        <Button variant='contained' color='green' onClick={() => {}} >
+          Generate Report
+        </Button>
+      </div>
+    </Container>
   )
 }
 
