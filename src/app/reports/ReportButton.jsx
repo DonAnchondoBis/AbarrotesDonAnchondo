177505670b16515957879579a5a02f3d6f6aebfa
Salvador511 <salvadororozcogon511@gmail.com>
2025-06-19 12:46:14 -0600

    feat: refactor ReportButton component to improve styling and data handling

--- src/app/reports/ReportButton.jsx
+++ src/app/reports/ReportButton.jsx
@@ -1,20 +1,59 @@
+'use client'
 import { PDFDownloadLink } from '@react-pdf/renderer'
 import { InventoryReportPdf } from './ReportsBase'
 import { Button } from '@mui/material'
 
-export const ReportButton = ({ category, data }) => {
+import { styled } from '@mui/material/styles'
+import getClassPrefixer from '~/app/UI/classPrefixer'
+
+const displayName = 'ShrinkageTable'
+const classes = getClassPrefixer(displayName)
+
+const Container = styled('div')(({ theme }) => ({
+  padding:'1rem 0',
+  [`& .${classes.button}`]: {
+    color: theme.palette.background.main,
+    textDecoration: 'none',
+  },
+  '& a': {
+    textDecoration: 'none',
+    color: 'inherit',
+    '&:hover': {
+      textDecoration: 'none',
+    },
+  }
+}))
+
+export const ReportButton = ({ category = '', data = [] }) => {
+  const hasValidData = data.length > 0
+  // TODO: DELETE THIS CONDITION WHEN WE HAVE REPORTS FOR ALL CATEGORIES
+  if (!hasValidData || category !== 'inventory') {
+    return (
+      <Button
+        variant="contained"
+        color="green"
+        disabled={true}
+      >
+        No data to generate report
+      </Button>
+    )
+  }
   return (
-    <div>
+    <Container>
       <PDFDownloadLink
         document={
           category === 'inventory' ? <InventoryReportPdf data={data} /> : <></>
         }
-        fileName={`InventoryReport_${new Date().toLocaleDateString()}.pdf`}
+        fileName={`${category}Report_${new Date().toLocaleDateString()}.pdf`}
       >
-        <Button variant="contained" color="green">
+        <Button
+          variant="contained"
+          color="green"
+          className={classes.button}
+        >
           Generate Report
         </Button>
       </PDFDownloadLink>
-    </div>
+    </Container>
   )
 }
