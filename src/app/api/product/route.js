42890428d92171bc7d379db4800ab9b9393103a7
Raul Muñiz <141065345+RaulMun@users.noreply.github.com>
2025-06-24 17:28:34 -0600

    feat/Cloudinary4Products  (#26)
    * feat: add image field to Product model and implement image upload in POST /api/product
    
    * feat: update Product to include an image with cloudinary
    
    * feat: rename image field in Product model to imageUrl for clarity
    
    * chore: remove image field from Product model and update related tests and routes for image handling
    
    * fix: adjust Content-Type header handling in apiFetch for FormData support
    
    ---------
    
    Co-authored-by: Salvador511 <salvadororozcogon511@gmail.com>

--- src/app/api/product/route.js
+++ src/app/api/product/route.js
@@ -5,10 +5,11 @@ import ERROR from '~/Libs/error'
 import cleanerData from '~/app/api/Libs/cleanerData'
 import validatorFields from '~/app/api/Libs/validatorFields'
 import prisma from '~/app/api/Libs/prisma'
+const CLOUDINARY_URL = process.env.CLOUDINARY_URL
 
 export const GET = async request => {
   try {
-    const { role = null, userId } = authenticateToken(request) ?? {}
+    const { role, userId } = authenticateToken(request) ?? {}
 
     if (!['ADMIN', 'WAREHOUSE', 'CASHIER'].includes(role) || !userId) return ERROR.FORBIDDEN()
 
@@ -18,27 +19,45 @@ export const GET = async request => {
     const response = products.map(product => cleanerData({ payload: product }))
     return NextResponse.json(response, { status: 200 })
   } catch (error) {
-    console.error('Error in GET /api/product:', error)
     return NextResponse.json({ error: error.message }, { status: error.status || 500 })
   }
 }
 
 export const POST = async request => {
   try {
-    const { role = null, userId } = authenticateToken(request) ?? {}
+    const { role, userId } = authenticateToken(request) ?? {}
     if (!['ADMIN', 'WAREHOUSE'].includes(role) || !userId) return ERROR.FORBIDDEN()
-    const data = await request.json()
+
+    const formData = await request.formData()
+    const file = formData.get('file')
+    if (!file) return ERROR.INVALID_FIELDS()
+
+    const data = JSON.parse(formData.get('data'))
+
     const isValid = validatorFields({ data, shape: Product.shape })
-    if (isValid){
-      const payload = await prisma.product.create({
-        data
-      })
-      const response = cleanerData({ payload })
-      return NextResponse.json(response, { status: 201 })
-    }
-    return ERROR.FORBIDDEN()
+    if (!isValid) return ERROR.INVALID_FIELDS()
+
+    const uploadForm = new FormData()
+    uploadForm.append('file', file)
+    uploadForm.append('upload_preset', 'unsignedV')
+    uploadForm.append('cloud_name', 'dhpnfud6f')
+
+    const res = await fetch(CLOUDINARY_URL, {
+      method: 'POST',
+      body: uploadForm
+    })
+    const { url } = await res.json()
+    if (!url) return ERROR.SERVICE_UNAVAILABLE()
+
+    const payload = await prisma.product.create({
+      data: {
+        imageUrl: url,
+        ...data,
+      }
+    })
+    const response = cleanerData({ payload })
+    return NextResponse.json(response, { status: 201 })
   } catch (error) {
-    console.error('Error in POST /api/product:', error)
     return NextResponse.json({ error: error.message }, { status: error.status || 500 })
   }
 }
