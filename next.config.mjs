2597297db8057a4ae8a18c490f7d1dcefb116fee
RaulMun <raulmuniz38@gmail.com>
2025-06-25 12:15:12 -0600

    feat: update Point Of Sale component to use Next.js Image component

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    domains: ['res.cloudinary.com'],
     remotePatterns: [
       {
         protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+      {
+        protocol: 'http',
         hostname: 'res.cloudinary.com',
       },
     ],
