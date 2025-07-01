3a303f07e5d5f11e9dd1462fbc01579b6c5452db
KamenDeku <alonsocorral2004@gmail.com>
2025-07-01 11:52:36 -0600

    feat: Update seed script to clear and populate database with new user and supplier data

--- prisma/seed.js
+++ prisma/seed.js
@@ -4,6 +4,16 @@ import bcrypt from 'bcrypt'
 const prisma = new PrismaClient()
 async function main() {
 
+  await prisma.ticketProduct.deleteMany({})
+  await prisma.ticket.deleteMany({})
+  await prisma.inventoryLog.deleteMany({})
+  await prisma.lot.deleteMany({})
+  await prisma.product.deleteMany({})
+  await prisma.storeInfo.deleteMany({})
+  await prisma.supplier.deleteMany({})
+  await prisma.user.deleteMany({})
+
+  //Users
   await Promise.all([
     prisma.user.upsert({
       where: { username: 'arturo' },
@@ -37,7 +47,7 @@ async function main() {
     }), 
   ])
   
-  //Productos
+  //Products
   const product = await Promise.all([
     prisma.product.upsert({
       where: { name: "Apple's" },
@@ -151,7 +161,7 @@ async function main() {
     }),
   ])
 
-  //Lotes
+  //Lots
   await Promise.all([
     product.map(p=>
       prisma.lot.create({
@@ -165,20 +175,54 @@ async function main() {
     )
   ])
     
-  //Proveedores
-  await prisma.supplier.create({
-    data: {
-      name: 'manuel',
-      phone: '111222333',
-      email: 'manuel@suppliers.com',
-    },
+  //Suppliers
+  await prisma.supplier.upsert({
+    where: { name: 'Potato' },
+    update: {},
+    create: [
+      {
+        name: 'Nestlé México',
+        phone: '800-123-4567',
+        email: 'ventas@nestle.com.mx',
+      },
+      {
+        name: 'Grupo Bimbo',
+        phone: '800-123-0001',
+        email: 'proveedores@bimbo.com',
+      },
+      {
+        name: 'La Moderna',
+        phone: '800-456-7890',
+        email: 'pedidos@lamoderna.com',
+      },
+      {
+        name: 'Sigma Alimentos',
+        phone: '800-321-4321',
+        email: 'contacto@sigmaalimentos.com',
+      },
+      {
+        name: 'Lala México',
+        phone: '800-000-5252',
+        email: 'contacto@lala.com.mx',
+      },
+      {
+        name: 'Gamesa',
+        phone: '800-555-2233',
+        email: 'proveedor@gamesa.com.mx',
+      },
+      {
+        name: 'Herdez',
+        phone: '800-456-9999',
+        email: 'ventas@herdez.com.mx',
+      },
+    ],
   })
 
-  //Tienda
+  //Store
   await prisma.storeInfo.create({
     data: {
       name: 'Abarrotes Don Anchondo',
-      address: 'Av. Revolución ',
+      address: 'Av. Rancho la Casita 123, Col. San Cienega de Ortiz',
       phone: '55-1234-5650',
       dollarValue: 18.5,
       yenValue: 0.13,
